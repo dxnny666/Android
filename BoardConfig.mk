76322e7c7f705d4bb93be375f27e24c09d8bdce9
dxnny666 <94081720+dxnny666@users.noreply.github.com>
2021-12-04 17:40:37 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,48 +1,45 @@
-TARGET_BOARD_PLATFORM := sdm845 # определяет платформу, на которой построено устройство (зависит от используемого процессора)
-# Architecture
-TARGET_ARCH := arm64 # основная архитектура процессора
-TARGET_ARCH_VARIANT := armv8-a # указывает на вариант архитектуры armv8-a
-TARGET_CPU_ABI := arm64-v8a # Application Binary Interface (https://developer.android.com/ndk/guides/abis набор поддерживаемых процессором команд)
+TARGET_BOARD_PLATFORM := sdm845 
+
+TARGET_ARCH := arm64 
+TARGET_ARCH_VARIANT := armv8-a 
+TARGET_CPU_ABI := arm64-v8a 
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a75 # указывает на тип ядра процессора
+TARGET_CPU_VARIANT := cortex-a75
 
-TARGET_2ND_ARCH := arm # указывает на вторую поддерживаемую архитектуру процессором
-TARGET_2ND_ARCH_VARIANT := armv8-a # указывает на вариант архитектуры armv8-a
-TARGET_2ND_CPU_ABI := armeabi-v7a # Application Binary Interface (https://developer.android.com/ndk/guides/abis набор поддерживаемых процессором команд)
-TARGET_2ND_CPU_ABI2 := armeabi # указывает на тип ядра процессора
-TARGET_2ND_CPU_VARIANT := cortex-a55 # указывает на тип ядра процессора
+TARGET_2ND_ARCH := arm 
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a 
+TARGET_2ND_CPU_ABI2 := armeabi 
+TARGET_2ND_CPU_VARIANT := cortex-a55
 
-# Kernel
-TARGET_KERNEL_SOURCE := kernel/mirea/it1 # указывает путь к исходному коду ядра.
-TARGET_KERNEL_CONFIG := it_defconfig # указывает имя конфигурационного файла для сборки ядра.
+TARGET_KERNEL_SOURCE := kernel/mirea/it1 
+TARGET_KERNEL_CONFIG := it_defconfig 
 
-# Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE := 96000000 # размер образа ядра в байтах
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0 # размер образа рекавери в байтах
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4000000000 # размер образа системы в байтах
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 1294991360 # размер образа для хранения данных пользователя в байтах
-BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 1500000000 # размер образа vendor
+BOARD_BOOTIMAGE_PARTITION_SIZE := 96000000 
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0 
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4000000000 
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1294991360 
+BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 1500000000 
 BOARD_HAS_NO_CACHE_PARTITION := true
 
-AB_OTA_UPDATER := true # активация режима обновления по схеме A/B
-AB_OTA_PARTITIONS := \ # список разделов, входящие в обновление по OTA
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS := \ 
 	boot \
 	system \
 	vendor
-PRODUCT_PACKAGES += \ # дополнительные пакеты, необходимые для установки A/B OTA
+PRODUCT_PACKAGES += \ 
 	update_engine \
 	update_verifier
-TARGET_NO_RECOVERY := true # отключение наличия раздела recovery
-BOARD_USES_RECOVERY_AS_BOOT := true # определение, что recovery встроено в ядро
+TARGET_NO_RECOVERY := true 
+BOARD_USES_RECOVERY_AS_BOOT := true 
 
-# Wifi related defines
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
-#BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+
 BOARD_HOSTAPD_DRIVER := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_wl12xx
-#BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+
 BOARD_WLAN_DEVICE := wlan0
 WIFI_DRIVER_MODULE_NAME := rt2800usb
 WIFI_DRIVER_MODULE_PATH := /system/lib/modules/rt2800usb.ko
